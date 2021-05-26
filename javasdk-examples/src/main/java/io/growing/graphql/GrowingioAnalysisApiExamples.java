41c1cb1a118b22a650ddfb865fcc59cecb2279b2
hhh-xiaoming <jiangboyu1997@163.com>
2021-05-26 11:16:50 +0800

    调整测试，完善readme

--- javasdk-examples/src/main/java/io/growing/graphql/GrowingioAnalysisApiExamples.java
+++ javasdk-examples/src/main/java/io/growing/graphql/GrowingioAnalysisApiExamples.java
@@ -14,16 +14,20 @@ import static io.growing.graphql.model.JobStageDto.ERROR;
 public class GrowingioAnalysisApiExaples {
 
     static String projectId = "WlGk4Daj"; //如果资源不在项目中，那么就没有projectId参数
-    final static GrowingioApi growingioApi = GrowingioApi.apply("http://uat-gdp.growingio.com/graphql", "Authorization", "fab69b1f-35c1-4cb5-81e8-ebe167ee2366");
+    final static GrowingioApi growingioApi = GrowingioApi.apply("http://uat-gdp.growingio.com/graphql", "Authorization", "8d1ec32c-116c-4f6e-a89e-dc6bd78d0266");
 
+    //设置对应的单图id和type
     public static void main(String[] args) {
-        AnalysisExportJobDto analysisChartsExportJobDto = growingioApi.submitAnalysisExportJob(projectId, "kqQewGry", AnalysisExportJobParamDto.builder().setAnalysisType(CHARTS).build(), "UTF-16LE");
+        submitJobTest("kqQewGry",AnalysisExportJobParamDto.builder().setAnalysisType(CHARTS).build());
+        submitJobTest("kqQewGry",AnalysisExportJobParamDto.builder().setAnalysisType(FUNNELS).build());
+        submitJobTest("kqQewGry",AnalysisExportJobParamDto.builder().setAnalysisType(RETENTIONS).build());
+        submitJobTest("kqQewGry",AnalysisExportJobParamDto.builder().setAnalysisType(FREQUENCIES).build());
+    }
+
+    public static void submitJobTest (String id,AnalysisExportJobParamDto type) {
+        AnalysisExportJobDto analysisChartsExportJobDto = growingioApi.submitAnalysisExportJob(projectId, id, type, "UTF-16LE");
         System.out.println(analysisChartsExportJobDto);
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }        boolean isDone = false;
+        boolean isDone = false;
         JobResultDto jobResultDto = JobResultDto.builder().build();
         while (!isDone){
             //set对应的id
